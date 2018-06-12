271cb2bb8e95a40ebf9c03c0834a9d4421026c7b
ping543f <md.saefullah@gmail.com>
2018-06-12 09:51:36 +0600

    offline summernote integration

--- index.php
+++ index.php
@@ -1,20 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>News Site</title>
+    <meta charset="UTF-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="summernote.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
+
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.js"></script>
-    <link href="css/summernote.css" rel="stylesheet">
-    <script src="js/summernote.js"></script>
+    <script src="summernote.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
 
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
 <div class="container">
     <center><h1> <span class="label label-default">Summernote data formatter</span></h1></center>
     <form name="summernote" method="post" action="setdata.php">
-        <textarea name="newsbody" id="summernote"></textarea>
+        <textarea name="newsbody" id="summernote" class="summernote"></textarea>
         <input type="submit" class="btn btn-success"/>
     </form>
     <br/>
